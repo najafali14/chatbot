c859fe26da13f3ec12ca604a2384e03bb177967e
Najaf Ali <65591681+najafali14@users.noreply.github.com>
2025-03-30 05:55:55 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from google import genai
+from google.genai import types
+import pathlib
+import httpx
+app = FastAPI()
+
+@app.get("/")
+def Chatbot(prompt):
+    client = genai.Client(api_key="AIzaSyBo2b6UyVbCepoxQwEgP91FFHx_v-bOAKI")
+
+# Retrieve the PDF
+    file_path = pathlib.Path('nazim.pdf')
+
+# Upload the PDF using the File API
+    sample_file = client.files.upload(
+  file=file_path,
+)
+
+    # prompt="Summarize this document"
+
+    response = client.models.generate_content(
+  model="gemini-1.5-flash",
+  contents=[sample_file, f"YOU are marketing assistant. you will give accurate answer on {prompt}. don't give extra explanation just give me exact answer read or thing"])
+    result = response.text
+    return result
